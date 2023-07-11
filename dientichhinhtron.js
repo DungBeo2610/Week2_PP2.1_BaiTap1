b97ca1ecc40e0ac01c06f02bbe8ac3d4893df346
DungBeo2610 <ducdungdo40@gmail.com>
2023-07-11 10:47:49 +0700

    baitap1.3

---
+++ dientichhinhtron.js
@@ -0,0 +1,6 @@
+inputPi = prompt("Enter the Pi");
+inputR =prompt("Enter the R");
+let Pi = parseInt(inputPi);
+let R = parseInt(inputR);
+let CircleArea = R*2*Pi;
+document.write("CircleArea="+CircleArea)
