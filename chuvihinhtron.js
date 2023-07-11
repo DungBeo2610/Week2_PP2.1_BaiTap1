370d5db77da0e3c3c92b335976f172443768d289
DungBeo2610 <ducdungdo40@gmail.com>
2023-07-11 10:51:35 +0700

    battap

--- chuvihinhtron.js
+++ chuvihinhtron.js
@@ -2,5 +2,5 @@ inputPi = prompt("Enter the Pi");
 inputR =prompt("Enter the R");
 let Pi = parseInt(inputPi);
 let R = parseInt(inputR);
-let CircleArea = R*R*Pi;
-document.write("CircleArea="+CircleArea)
+let diameter = R*2*Pi;
+document.write("diameter="+diameter)
